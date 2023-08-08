151858bd690763fc06b42098130371a1ca569b38
Alexp163 <dr.p163@ya.ru>
2023-08-08 22:33:09 +0400

    исправление html(регистрация и авторизация) 08.08.23(22.30)

--- app/avtouser_app/urls.py
+++ app/avtouser_app/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import avtouser, autorisation_user, registration_user
-
+from .views import avtouser, autorisation_user, registration_user, register
 
 urlpatterns = [
     path('', avtouser),
     path('autorisation/', autorisation_user),
     path('registration/', registration_user),
-
+    path('register/', register, name='register')
 ]
--- app/avtouser_app/views.py
+++ app/avtouser_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from avtouser_app.models import MyBase
+
 
 def avtouser(request):
     return render(request, 'avtouser_app/index.html')
@@ -11,3 +13,12 @@ def autorisation_user(request):
 
 def registration_user(request):
     return render(request, "avtouser_app/registration.html")
+
+def register(request):
+    login = request.POST.get('login')
+    password1 = request.POST.get('password1')
+    password2 = request.POST.get('password2')
+    if password1 != password2:
+        return render(request, "avtouser_app/registration.html")
+    MyBase.objects.get_or_create(login=login, password=password1)
+    return render(request, "avtouser_app/index.html")
